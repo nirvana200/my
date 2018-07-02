5de253b7e545550d6c38de0855692fa9f4788883
nirvana200 <nirvana200@naver.com>
2018-07-02 16:43:29 +0900

    최초 커밋

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int temp1 = 123;
+		String temp2 = "123";
+		Object temp3 = "";
+		
+		System.out.println( Integer.parseInt( (String)temp3 ) );
+		
+	}
+
+}
